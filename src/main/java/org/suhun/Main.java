c4b89e3d9982a32831914b4f007424b5f0271e07
suhunlin <lin.suhun@msa.hinet.net>
2023-11-30 12:28:13 +0800

    Add teacher method

--- src/main/java/org/suhun/Main.java
+++ src/main/java/org/suhun/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {8, 2, 6, 10, 4, 80, 20};
         SelectionSortImplement selectionSortImplement = new SelectionSortImplement();
-        int[] result = selectionSortImplement.selection_sort(nums);
+//        int[] result = selectionSortImplement.selection_sort(nums);
+        int [] result = selectionSortImplement.selection_sort_teacher(nums);
         for(int num:result){
             System.out.println(num);
         }
--- src/main/java/org/suhun/SelectionSortImplement.java
+++ src/main/java/org/suhun/SelectionSortImplement.java
@@ -20,6 +20,19 @@ public class SelectionSortImplement {
         return nums;
     }
 
+    public int[] selection_sort_teacher(int[] nums){
+        for(int i_start = 0; i_start <nums.length; i_start++){
+            int i_max = i_start;
+            for(int i_run = i_start + 1; i_run < nums.length; i_run++){
+                if(nums[i_run] > nums[i_max]){
+                    i_max = i_run;
+                }
+            }
+            swap(nums, i_start, i_max);
+        }
+        return nums;
+    }
+
     private void swap(int[] nums, int left_index, int right_index){
         int temp = nums[left_index];
         nums[left_index] = nums[right_index];
